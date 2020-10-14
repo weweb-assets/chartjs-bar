da076dd68b83c8212ce2112d5608955d256383d5
Aurélie VIOLETTE <aurelie@weweb.io>
2020-10-14 15:55:06 +0200

    Version 1.0.0

---
+++ src/wwSection.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="ww-base">
+        <wwObject v-if="content.background" isBackground v-bind="content.background" class="background"></wwObject>
+        <wwContainer class="content" v-bind="container" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: '__COMPONENT_NAME__',
+    props: {
+        content: Object,
+    },
+    wwDefaultContent: {
+        background: { isWwObject: true, type: 'ww-color' },
+        container: { isWwObject: true, type: 'ww-container' },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.ww-base {
+    position: relative;
+    min-height: 120px;
+    box-sizing: border-box;
+    .background {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+    }
+    .content {
+        height: 100%;
+        width: 100%;
+    }
+}
+</style>
