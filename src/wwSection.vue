15a3eaed3b96a90293c2065c807552d19dcc5e96
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2021-09-30 17:57:26 +0200

    V1

--- src/wwSection.vue
+++ src/wwSection.vue
@@ -1,19 +1,70 @@
 <template>
-    <wwLayout path="wwObjects" class="section-base"></wwLayout>
+    <div class="chart-container" style="position: relative">
+        <canvas id="chartjs-vertical-bar"></canvas>
+    </div>
 </template>
 
 <script>
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
+
 export default {
     props: {
         content: { type: Object, required: true },
+    },
+    data() {
+        return {
+            config: {
+                type: 'bar',
+                data: {
+                    labels: ['Tatooine', 'Coruscant', 'Kashyyyk', 'Dagobah', 'Bespin', 'Endor', 'Hoth'],
+                    datasets: [...this.content.datasets],
+                },
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                },
+            },
+            chartInstance: null,
+        };
+    },
+    watch: {
+        'content.datasets'(data) {
+            if (this.chartInstance) this.chartInstance.destroy();
+            console.log(data);
+            this.initChart();
+        },
+        'content.labels'(data) {
+            if (this.chartInstance) this.chartInstance.destroy();
+            console.log(data);
+            this.initChart();
+        },
+    },
+    mounted() {
+        if (this.chartInstance) this.chartInstance.destroy();
+        this.initChart();
+    },
+    beforeUnmount() {
+        this.chartInstance.destroy();
+    },
+    methods: {
+        initChart() {
+            this.chartInstance = new Chart(
+                wwLib.getFrontDocument().getElementById('chartjs-vertical-bar'),
+                this.config
+            );
+        },
     },
 };
 </script>
 
 <style lang="scss" scoped>
-.section-base {
-    display: flex;
-    flex-direction: column;
-    align-items: stretch;
+.chart-container {
+    width: inherit;
+    height: inherit;
+    #chartjs-vertical-bar {
+        width: 100% !important;
+        height: 100% !important;
+    }
 }
 </style>
--- ww-config.js
+++ ww-config.js
@@ -1,13 +1,58 @@
 export default {
-    "editor": {
-        "label": {
-            "fr": "Base",
-            "en": "Base"
+    editor: {
+        label: {
+            fr: 'Base',
+            en: 'Base',
         },
     },
     properties: {
-        wwObjects: {
-            hidden: true,
-            defaultValue: []
-        }}
-}
+        datasets: {
+            label: {
+                en: 'Datasets',
+                fr: 'Datasets',
+            },
+            type: 'Info',
+            options: {
+                text: { en: 'Datasets' },
+            },
+            bindable: 'repeatable',
+            defaultValue: [],
+        },
+        label: {
+            label: { en: 'Label', fr: 'Label' },
+            type: 'Info',
+            options: {
+                text: 'Label',
+            },
+            bindable: true,
+        },
+        backgroundColor: {
+            label: { en: 'Background color', fr: 'Background color' },
+            type: 'Info',
+            options: {
+                text: 'rgb(255, 99, 132)',
+            },
+            bindable: true,
+        },
+        borderColor: {
+            label: { en: 'Border color', fr: 'Border color' },
+            type: 'Info',
+            options: {
+                text: 'rgb(255, 99, 132)',
+            },
+            bindable: true,
+        },
+        data: {
+            label: {
+                en: 'Data',
+                fr: 'Data',
+            },
+            type: 'Info',
+            options: {
+                text: { en: 'Data set' },
+            },
+            bindable: true,
+            defaultValue: [],
+        },
+    },
+};
