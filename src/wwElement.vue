31d39034d53a55088733bb03dab0f31a6d243262
Dams-d <damien.duro8@gmail.com>
2022-04-04 19:20:52 +0200

    Update Chart.JS element

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -50,11 +50,164 @@ export default {
     },
     computed: {
         config() {
+            let labels = [];
+            let datasets = [];
+
+            if (this.content.dataType === 'no-code') {
+                labels = undefined;
+                const data =
+                    (!this.content.data || Array.isArray(this.content.data)
+                        ? this.content.data
+                        : this.content.data.data) || [];
+
+                const groupBy = this.content.groupBy;
+                const groupByProperty = this.content.groupByProperty;
+                const dataXField = this.content.dataXField;
+                const dataXFieldProperty = this.content.dataXFieldProperty;
+                const dataYField =
+                    this.content.yAxis === 'record-count' ? this.content.dataXField : this.content.dataYField;
+                const dataYFieldProperty = this.content.dataYFieldProperty;
+                const aggregate = this.content.yAxis === 'record-count' ? 'record-count' : this.content.aggregate;
+                const colors = this.content.colors;
+                if (!groupBy) {
+                    datasets = [{ label: dataXField, backgroundColor: colors[0], data: [] }];
+                    if (!data.length || !Array.isArray(_.get(data[0], dataXField))) {
+                        datasets[0].data = data.map(item => ({
+                            x: _.get(item, dataXField),
+                            y: this.aggregate(
+                                aggregate,
+                                data
+                                    .filter(elem => _.get(elem, dataYField) === _.get(item, dataYField))
+                                    .map(elem => _.get(elem, dataYField))
+                            ),
+                        }));
+                    } else {
+                        const arrayValues = [
+                            ...new Set(
+                                data
+                                    .map(item => {
+                                        const result = _.get(item, dataXField, []);
+                                        return Array.isArray(result)
+                                            ? result.map(elem => _.get(elem, dataXFieldProperty, elem))
+                                            : result;
+                                    })
+                                    .flat()
+                            ),
+                        ];
+                        datasets[0].data = arrayValues.map(arrayValue => ({
+                            x: arrayValue,
+                            y: this.aggregate(
+                                aggregate,
+                                data
+                                    .filter(item => {
+                                        let result = _.get(item, dataXField, []);
+                                        result = Array.isArray(result)
+                                            ? result.map(elem => _.get(elem, dataXFieldProperty, elem))
+                                            : result;
+                                        return result.includes(arrayValue);
+                                    })
+                                    .map(item => {
+                                        const result = _.get(item, dataYField, []);
+                                        return Array.isArray(result)
+                                            ? result.map(elem => _.get(elem, dataYFieldProperty, elem))
+                                            : result;
+                                    })
+                            ),
+                        }));
+                    }
+                } else {
+                    const groupByValues = [
+                        ...new Set(
+                            data
+                                .map(item => {
+                                    const result = _.get(item, groupBy, []);
+                                    return Array.isArray(result)
+                                        ? result.map(elem => _.get(elem, groupByProperty, elem))
+                                        : result;
+                                })
+                                .flat()
+                        ),
+                    ];
+                    const t = [
+                        ...new Set(
+                            data
+                                .map(item => {
+                                    const result = _.get(item, dataXField, []);
+                                    return Array.isArray(result)
+                                        ? result.map(elem => _.get(elem, dataXFieldProperty, elem))
+                                        : result;
+                                })
+                                .flat()
+                        ),
+                    ];
+                    datasets = groupByValues.map((groupByValue, index) => ({
+                        label: groupByValue,
+                        backgroundColor: colors[index % colors.length],
+                        data: t.map(elem => ({
+                            x: elem,
+                            y: this.aggregate(
+                                aggregate,
+                                data
+                                    .filter(item => {
+                                        const currentDataXFieldValue = _.get(item, dataXField);
+                                        const currentGroupByValue = _.get(item, groupBy);
+                                        return (
+                                            (Array.isArray(currentDataXFieldValue)
+                                                ? currentDataXFieldValue
+                                                      .map(elem => _.get(elem, dataXFieldProperty, elem))
+                                                      .includes(elem)
+                                                : currentDataXFieldValue === elem) &&
+                                            (Array.isArray(currentGroupByValue)
+                                                ? currentGroupByValue
+                                                      .map(elem => _.get(elem, groupByProperty, elem))
+                                                      .includes(groupByValue)
+                                                : currentGroupByValue === groupByValue)
+                                        );
+                                    })
+                                    .map(item => {
+                                        const result = _.get(item, dataYField);
+                                        return Array.isArray(result)
+                                            ? result.map(elem => _.get(elem, dataYFieldProperty, elem))
+                                            : result;
+                                    })
+                            ),
+                        })),
+                    }));
+                }
+
+                // Empty values
+                if (this.content.dataXEmpty === false) {
+                    for (const item of datasets) {
+                        item.data = item.data.filter(item => item.y && item.x);
+                    }
+                }
+                // Change axis
+                if (this.content.axis === 'y') {
+                    for (const item of datasets) {
+                        item.data = item.data.map(item => ({ x: item.y, y: item.x }));
+                    }
+                }
+                // Order by
+                for (const item of datasets) {
+                    item.data.sort((a, b) => {
+                        const field = this.content.dataOrderBy;
+                        return (
+                            (typeof a[field] === 'string' ? a[field].localeCompare(b[field]) : a[field] - b[field]) *
+                            (this.content.dataDirection === 'DESC' ? -1 : 1)
+                        );
+                    });
+                    item.data = item.data.map(item => ({ x: `${item.x}`, y: item.y }));
+                }
+            } else {
+                labels = this.content.labels;
+                datasets = this.content.datasets;
+            }
+
             return {
                 type: 'bar',
                 data: {
-                    labels: this.content.labels,
-                    datasets: this.content.datasets,
+                    labels,
+                    datasets,
                 },
                 options: {
                     indexAxis: this.content.axis,
@@ -77,6 +230,7 @@ export default {
                         },
                         y: {
                             stacked: this.content.stacked,
+                            beginAtZero: this.content.startAtZero,
                         },
                     },
                 },
@@ -85,17 +239,30 @@ export default {
     },
     watch: {
         config() {
-            if (
-                this.chartInstance &&
-                this.content.labels &&
-                this.content.labels.length &&
-                this.content.datasets &&
-                this.content.datasets.length
-            ) {
-                this.chartInstance.data.labels = this.content.labels;
-                this.chartInstance.data.datasets = this.content.datasets;
-                this.chartInstance.update();
-            }
+            // if (
+            //     this.chartInstance &&
+            //     this.content.labels &&
+            //     this.content.labels.length &&
+            //     this.content.datasets &&
+            //     this.content.datasets.length
+            // ) {
+            //     this.chartInstance.data.labels = this.content.labels;
+            //     this.chartInstance.data.datasets = this.content.datasets;
+            //     this.chartInstance.update();
+            // }
+            // this.chartInstance.data.labels = this.config.data.labels;
+            // this.chartInstance.data.datasets = this.config.data.datasets;
+            // this.chartInstance.update();
+        },
+        'config.data.datasets'() {
+            this.chartInstance.data.datasets = this.config.data.datasets;
+            if (this.chartInstance) this.chartInstance.destroy();
+            this.initChart();
+            this.chartInstance.update();
+        },
+        'config.data.labels'() {
+            this.chartInstance.data.labels = this.config.data.labels;
+            this.chartInstance.update();
         },
         'content.legendPosition'() {
             this.chartInstance.options.plugins.legend.position = this.content.legendPosition;
@@ -113,6 +280,22 @@ export default {
             if (this.chartInstance) this.chartInstance.destroy();
             this.initChart();
         },
+        'content.startAtZero'() {
+            if (this.chartInstance) this.chartInstance.destroy();
+            this.initChart();
+        },
+        'content.dataYField'() {
+            // eslint-disable-next-line vue/require-explicit-emits
+            this.$emit('update:content:effect', { dataYFieldProperty: null });
+        },
+        'content.dataXField'() {
+            // eslint-disable-next-line vue/require-explicit-emits
+            this.$emit('update:content:effect', { dataXFieldProperty: null });
+        },
+        'content.groupBy'() {
+            // eslint-disable-next-line vue/require-explicit-emits
+            this.$emit('update:content:effect', { groupByProperty: null });
+        },
     },
     mounted() {
         this.initChart();
@@ -121,9 +304,39 @@ export default {
         this.chartInstance.destroy();
     },
     methods: {
+        aggregate(operator, data) {
+            if (!data) return undefined;
+            switch (operator) {
+                case 'record-count':
+                    return [data].flat().length;
+                case 'distinct':
+                    return [...new Set([data].flat())].length;
+                case 'sum':
+                    return this.sum([data].flat());
+                case 'average':
+                    return this.average([data].flat());
+                case 'median':
+                    return this.median([data].flat());
+                case 'min':
+                    return Math.min(...[data].flat());
+                case 'max':
+                    return Math.max(...[data].flat());
+            }
+        },
         initChart() {
             const element = this.$el.querySelector('.chartjs-bar');
             this.chartInstance = new Chart(element, this.config);
+        },
+        median(arr) {
+            const mid = Math.floor(arr.length / 2),
+                nums = [...arr].sort((a, b) => a - b);
+            return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+        },
+        sum(arr) {
+            return arr.reduce((a, b) => a + b, 0);
+        },
+        average(arr) {
+            return arr.reduce((a, b) => a + b, 0) / arr.length;
         },
     },
 };
--- ww-config.js
+++ ww-config.js
@@ -1,24 +1,18 @@
 export default {
     editor: {
-        label: {
-            fr: 'Chart - Bar',
-            en: 'Chart - Bar',
-        },
+        label: 'Chart - Bar',
         icon: 'fontawesome/regular/chart-bar',
     },
     properties: {
         legendPosition: {
-            label: {
-                en: 'Legend position',
-                fr: 'Legend position',
-            },
+            label: 'Legend position',
             type: 'TextSelect',
             options: {
                 options: [
-                    { value: 'top', label: { en: 'Top' } },
-                    { value: 'bottom', label: { en: 'Bottom' } },
-                    { value: 'left', label: { en: 'Left' } },
-                    { value: 'right', label: { en: 'Right' } },
+                    { value: 'top', label: 'Top' },
+                    { value: 'bottom', label: 'Bottom' },
+                    { value: 'left', label: 'Left' },
+                    { value: 'right', label: 'Right' },
                 ],
             },
             bindable: true,
@@ -26,16 +20,13 @@ export default {
             defaultValue: 'top',
         },
         legendAlignement: {
-            label: {
-                en: 'Legend alignment',
-                fr: 'Legend alignement',
-            },
+            label: 'Legend alignment',
             type: 'TextSelect',
             options: {
                 options: [
-                    { value: 'start', label: { en: 'Start' } },
-                    { value: 'center', label: { en: 'Center' } },
-                    { value: 'end', label: { en: 'End' } },
+                    { value: 'start', label: 'Start' },
+                    { value: 'center', label: 'Center' },
+                    { value: 'end', label: 'End' },
                 ],
             },
             bindable: true,
@@ -43,7 +34,7 @@ export default {
             defaultValue: 'center',
         },
         axis: {
-            label: { en: 'Axis', fr: 'Axis' },
+            label: 'Axis',
             type: 'BigIconRadioGroup',
             options: {
                 choices: [
@@ -57,36 +48,47 @@ export default {
         },
         stacked: {
             type: 'OnOff',
-            label: {
-                en: 'Stacked bars',
-                fr: 'Stacked bars',
-            },
+            label: 'Stacked bars',
             bindable: true,
             responsive: true,
             defaultValue: false,
         },
-        labels: {
-            label: {
-                en: 'Labels',
-                fr: 'Labels',
+        startAtZero: {
+            label: 'Start at zero',
+            type: 'OnOff',
+            bindable: true,
+            responsive: true,
+            defaultValue: true,
+        },
+        dataType: {
+            label: 'Configuration',
+            type: 'TextSelect',
+            options: {
+                options: [
+                    { value: 'no-code', label: 'No-code' },
+                    { value: 'custom', label: 'Custom (dev-friendly)' },
+                ],
             },
+            section: 'settings',
+            defaultValue: 'custom',
+        },
+        labels: {
+            label: 'Labels',
             type: 'Info',
             options: {
-                text: { en: 'Chart labels' },
+                text: 'Chart labels',
             },
             responsive: true,
             section: 'settings',
             bindable: 'list',
             defaultValue: ['Tatooine', 'Coruscant', 'Kashyyyk', 'Dagobah', 'Bespin', 'Endor', 'Hoth'],
+            hidden: content => content.dataType !== 'custom',
         },
         datasets: {
-            label: {
-                en: 'Datasets',
-                fr: 'Datasets',
-            },
+            label: 'Datasets',
             type: 'Info',
             options: {
-                text: { en: 'Chart Datasets' },
+                text: 'Chart Datasets',
             },
             responsive: true,
             section: 'settings',
@@ -103,6 +105,235 @@ export default {
                     data: [26, 42, 35, 48, 52, 24, 75],
                 },
             ],
+            hidden: content => content.dataType !== 'custom',
+        },
+        data: {
+            label: {
+                en: 'Data',
+                fr: 'Data',
+            },
+            type: 'Info',
+            options: {
+                text: 'Chart data',
+            },
+            responsive: true,
+            section: 'settings',
+            bindable: 'list',
+            defaultValue: null,
+            hidden: content => content.dataType !== 'no-code',
+        },
+        xAxisTitle: {
+            label: 'X-axis',
+            section: 'settings',
+            hidden: content => content.dataType !== 'no-code' || !content.data,
+        },
+        dataXField: {
+            label: 'Field',
+            type: 'ObjectPropertyPath',
+            options: content => {
+                const data = (!content.data || Array.isArray(content.data) ? content.data : content.data.data) || [];
+                if (!Array.isArray(data) || !data[0]) return null;
+                return { object: data[0] };
+            },
+            section: 'settings',
+            defaultValue: null,
+            hidden: content => content.dataType !== 'no-code' || !content.data,
+        },
+        dataXFieldProperty: {
+            label: 'Field property',
+            type: 'ObjectPropertyPath',
+            options: content => {
+                const data = (!content.data || Array.isArray(content.data) ? content.data : content.data.data) || [];
+                if (!Array.isArray(data) || !data.length) return true;
+                const field = _.get(data[0], content.dataXField);
+                return !Array.isArray(field) || !field.length ? null : { object: field[0] };
+            },
+            defaultValue: null,
+            section: 'settings',
+            hidden: content => {
+                const data = (!content.data || Array.isArray(content.data) ? content.data : content.data.data) || [];
+                if (!Array.isArray(data) || !data[0]) return true;
+                const field = _.get(data[0], content.dataXField);
+                return (
+                    content.dataType !== 'no-code' ||
+                    !content.data ||
+                    !Array.isArray(field) ||
+                    !field.length ||
+                    typeof field[0] !== 'object'
+                );
+            },
+        },
+        dataOrderBy: {
+            label: 'Order by',
+            type: 'TextRadioGroup',
+            options: {
+                choices: [
+                    { value: 'x', label: 'X value' },
+                    { value: 'y', label: 'Y value' },
+                ],
+            },
+            section: 'settings',
+            defaultValue: 'x',
+            hidden: content => content.dataType !== 'no-code' || !content.data,
+        },
+        dataDirection: {
+            type: 'TextRadioGroup',
+            options: {
+                choices: [
+                    { value: 'ASC', label: 'Ascending' },
+                    { value: 'DESC', label: 'Descending' },
+                ],
+            },
+            section: 'settings',
+            defaultValue: 'ASC',
+            hidden: content => content.dataType !== 'no-code' || !content.data,
+        },
+        dataXEmpty: {
+            label: 'Include empty values',
+            type: 'OnOff',
+            section: 'settings',
+            defaultValue: false,
+            hidden: content => content.dataType !== 'no-code' || !content.data,
+        },
+        yAxisTitle: {
+            label: {
+                en: 'Y-axis',
+                fr: 'Y-axis',
+            },
+            section: 'settings',
+            hidden: content => content.dataType !== 'no-code' || !content.data,
+        },
+        yAxis: {
+            type: 'TextRadioGroup',
+            options: {
+                choices: [
+                    { value: 'record-count', label: 'Record count' },
+                    { value: 'field-summary', label: 'Field summary' },
+                ],
+            },
+            section: 'settings',
+            defaultValue: 'record-count',
+            hidden: content => content.dataType !== 'no-code' || !content.data,
+        },
+        dataYField: {
+            label: 'Field',
+            type: 'ObjectPropertyPath',
+            options: content => {
+                const data = (!content.data || Array.isArray(content.data) ? content.data : content.data.data) || [];
+                if (!Array.isArray(data) || !data[0]) return null;
+                return { object: data[0] };
+            },
+            section: 'settings',
+            defaultValue: null,
+            hidden: content => content.dataType !== 'no-code' || !content.data || content.yAxis !== 'field-summary',
+        },
+        dataYFieldProperty: {
+            label: 'Field property',
+            type: 'ObjectPropertyPath',
+            options: content => {
+                const data = (!content.data || Array.isArray(content.data) ? content.data : content.data.data) || [];
+                if (!Array.isArray(data) || !data.length) return true;
+                const field = _.get(data[0], content.dataYField);
+                return !Array.isArray(field) || !field.length ? null : { object: field[0] };
+            },
+            defaultValue: null,
+            section: 'settings',
+            hidden: content => {
+                const data = (!content.data || Array.isArray(content.data) ? content.data : content.data.data) || [];
+                if (!Array.isArray(data) || !data.length) return true;
+                const field = _.get(data[0], content.dataYField);
+                return (
+                    content.dataType !== 'no-code' ||
+                    !content.data ||
+                    content.yAxis !== 'field-summary' ||
+                    !Array.isArray(field) ||
+                    !field.length ||
+                    typeof field[0] !== 'object'
+                );
+            },
+        },
+        aggregate: {
+            label: 'Aggregate',
+            type: 'TextSelect',
+            options: content => {
+                const data = (!content.data || Array.isArray(content.data) ? content.data : content.data.data) || [];
+                let field = _.get(data[0], content.dataYField);
+                if (Array.isArray(field) && field.length) field = _.get(field[0], content.dataYFieldProperty);
+                const isNumber = Number.isFinite(data[0] && content.dataYField && field);
+                return {
+                    placeholder: 'Select',
+                    options: [
+                        { value: 'distinct', label: 'Distinct' },
+                        isNumber ? { value: 'sum', label: 'Sum' } : null,
+                        isNumber ? { value: 'average', label: 'Average' } : null,
+                        isNumber ? { value: 'median', label: 'Median' } : null,
+                        isNumber ? { value: 'min', label: 'Min' } : null,
+                        isNumber ? { value: 'max', label: 'Max' } : null,
+                    ].filter(Boolean),
+                };
+            },
+            section: 'settings',
+            defaultValue: 'distinct',
+            hidden: content => content.dataType !== 'no-code' || !content.data || content.yAxis !== 'field-summary',
+        },
+        groupBy: {
+            label: 'Group by',
+            type: 'ObjectPropertyPath',
+            options: content => {
+                const data = (!content.data || Array.isArray(content.data) ? content.data : content.data.data) || [];
+                if (!Array.isArray(data) || !data[0]) return null;
+                return { object: data[0] };
+            },
+            section: 'settings',
+            defaultValue: null,
+            hidden: content =>
+                content.dataType !== 'no-code' ||
+                !content.data ||
+                (content.yAxis !== 'record-count' && content.aggregate !== 'distinct' && content.aggregate !== 'sum'),
+        },
+        groupByProperty: {
+            label: 'Group by property',
+            type: 'ObjectPropertyPath',
+            options: content => {
+                const data = (!content.data || Array.isArray(content.data) ? content.data : content.data.data) || [];
+                if (!Array.isArray(data) || !data.length) return true;
+                const field = _.get(data[0], content.groupBy);
+                return !Array.isArray(field) || !field.length ? null : { object: field[0] };
+            },
+            defaultValue: null,
+            section: 'settings',
+            hidden: content => {
+                const data = (!content.data || Array.isArray(content.data) ? content.data : content.data.data) || [];
+                if (!Array.isArray(data) || !data[0]) return true;
+                const field = _.get(data[0], content.groupBy);
+                return (
+                    content.dataType !== 'no-code' ||
+                    !content.data ||
+                    !Array.isArray(field) ||
+                    !field.length ||
+                    typeof field[0] !== 'object'
+                );
+            },
+        },
+        colors: {
+            label: 'Colors',
+            type: 'Array',
+            section: 'settings',
+            options: {
+                item: {
+                    type: 'Color',
+                },
+            },
+            defaultValue: [
+                'rgba(255, 99, 132)',
+                'rgba(54, 162, 235)',
+                'rgba(255, 206, 86)',
+                'rgba(75, 192, 192)',
+                'rgba(153, 102, 255)',
+                'rgba(255, 159, 64)',
+            ],
+            bindable: true,
+            hidden: content => content.dataType !== 'no-code' || !content.data,
         },
     },
 };
